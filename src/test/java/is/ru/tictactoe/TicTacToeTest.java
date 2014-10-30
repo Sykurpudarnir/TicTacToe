0d4b2a0f5a9692e5d2748e2bbeacbf1d0595299d
johannai13 <johannai13@ru.is>
2014-10-30 16:31:05 +0000

    fleiri test gerd fyrir winner

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -31,17 +31,34 @@ public class TicTacToeTest{
 		}
 
 		@Test
-		public void testWinner()
+		public void testWinnerLost()
+		{
+			char[][] board2 = new char[][]{{'X', 'O', 'X'},{'X','O','O'},{'O','X','X'}};
+			char[][] board4 = new char[][]{{'O', 'O', 'X'},{' ','X',' '},{' ',' ',' '}};
+			assertEquals(3, TicTacToe.checkIfWon(board2));
+			assertEquals(3, TicTacToe.checkIfWon(board4));
+		}
+
+		@Test
+		public void testWinnerDiag()
 		{
 			char[][] board1 = new char[][]{{'X', 'O', 'O'},{'X','X','O'},{'O','X','X'}};
-			char[][] board2 = new char[][]{{'X', 'O', 'X'},{'X','O','O'},{'O','X','X'}};
 			char[][] board3 = new char[][]{{'O', 'O', 'X'},{' ','X',' '},{'X',' ',' '}};
-			char[][] board4 = new char[][]{{'O', 'O', 'X'},{' ','X',' '},{' ',' ',' '}};
-			char[][] board5 = new char[][]{{'O', 'O', 'X'},{'O','X','X'},{'O','X',' '}};
 			assertEquals(1, TicTacToe.checkIfWon(board1));
-			assertEquals(3, TicTacToe.checkIfWon(board2));
 			assertEquals(1, TicTacToe.checkIfWon(board3));
-			assertEquals(3, TicTacToe.checkIfWon(board4));
+		}
+
+		@Test
+		public void testWinnerCol()
+		{
+			char[][] board5 = new char[][]{{'O', 'O', 'X'},{'O','X','X'},{'O','X',' '}};
 			assertEquals(2, TicTacToe.checkIfWon(board5));
+		}
+
+		@Test
+		public void testWinnerRow()
+		{
+			char[][] board6 = new char[][]{{'O', 'O', 'X'},{'X','X','X'},{'O','X','O'}};
+                        assertEquals(1, TicTacToe.checkIfWon(board6));
 		}
 }
