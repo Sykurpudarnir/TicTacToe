86ea800721aeff50d0993fb0bc78735605f63302
Sólveig Sif Guðmundsdóttir <solveig@i-3a29db7a.greenqloud.internal>
2014-11-02 12:45:18 +0000

    test fyrir its a draw

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -4,20 +4,7 @@ import java.util.Scanner;
 public class TicTacToe{
 
 	public static int count = 1;
-	public static int moves = 1;
 
-	public static char getSymbol() {
-		if(moves % 2 == 0) {
-			return 'O';
-		} else {
-		return 'X';
-	}
-
-	public static void makeMove(char[][] board, row, column) {
-		char sym = getSymbol();
-		board[row][column] = sym;
-		moves++;
-	}
 
 	public static boolean checkIfWon(char[][] board){
         if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
